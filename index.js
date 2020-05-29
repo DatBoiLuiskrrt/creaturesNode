e8eba0f88bf0592d18dea342fd31578efaa10c79
Luis Hernandez <datboiluiskrrt@gmail.com>
2020-05-28 20:22:48 -0500

    Import my routes

--- index.js
+++ index.js
@@ -1,9 +1,11 @@
 const express = require('express');
+const racesRoutes = require("./races/racesRoute");
 const server = express();
-server.use('/', (req,res) => {
-    res.status(200).send("Hello from express app running");
-});
+server.use('/races', racesRoutes); 
 
+server.use("/", (req,  res) => {
+    res.status(200).send("hello from express");
+})
 server.listen(8000, () => {
     console.log("magic happening on port 8000");
 });
